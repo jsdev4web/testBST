5b1d7690f4d36045e42e732ef920925d60053691
jsweb4dev <jsweb4dev@gmail.com>
2024-09-30 20:03:43 -0400

    update to bst

--- myscript.js
+++ myscript.js
@@ -7,28 +7,28 @@ class Node {
     }
 }
 
-class BinarySearchTree{
-    constructor(nums){
-        this.nums = nums
+class BinarySearchTree {
+    constructor(nums2){
+        this.nums = nums2
         this.root = null
     }
 
-    buildtree(nums) {
+    buildtree(nums2) {
 
-        if (nums.length <= 0) { return null} 
+        if (nums2.length <= 0) { return null} 
 
         let root = this.root
         //console.log(root)  null
-        let mid = Math.floor(nums.length / 2)
+        let mid = Math.floor(nums2.length / 2)
         //console.log(nums[mid])3-just mid and 4-mid nums
 
-        root = new Node(nums[mid])
+        root = new Node(nums2[mid])
         //console.log(root)
 
-        let lSide = nums.slice(0, mid)
+        let lSide = nums2.slice(0, mid)
         console.log(lSide, "left side, unused")
 
-        let rSide = nums.slice(mid  + 1, nums.length)
+        let rSide = nums2.slice(mid  + 1, nums2.length)
         console.log(rSide, "right side unused")
         
         console.log(lSide.length, rSide.length)
@@ -38,56 +38,140 @@ class BinarySearchTree{
         let totalLength = lSide.length + rSide.length
         console.log(totalLength)
 
-        let i = 0;
 
-
-        while(totalLength >= i){
-            i++ //console.log(i) I got 7 loops through
+        for(let i = 0; i <= lSide.length; i++){
+           //console.log(i) I got 7 loops through
 
             //if left side greater 3 and not = null
-            if (lSide.length >= 3 && lSide.length != null){
+            if (lSide.length >= 3){
                 //Get the middle of the left side
-
-                let lMid = Math.ceil(lSide.length / 2)
+                let lMid = Math.floor(lSide.length / 2)
                 //console.log(lMid)
+                let parent = new Node(lSide[lMid])
+                //console.log(root)
+                //console.log(parent)
+                root.left = parent  
+                //console.log(lSide)
+                //console.log(lSide)
+            } 
+            if (lSide.length <= 3){
+                if(lSide[0] < lSide[2]){
                 
+                    //create a node for leaf 
+                    let childL = new Node(lSide[0])
+                    let childR = new Node(lSide[2])
+                    //set parent node to left
+                    let parent = root.left
+                    //assign the chile node
+                    parent.left = childL
+                    parent.right = childR
+                    
+                } else{
+                    
+                    let childR = new Node(lSide[0])
+                    let childL = new Node(lSide[2])
+                    let parent = root.right
+                    parent.right = childR
+                    parent.left = childL
+                    
+                } 
+            } else{
+                //console.log(root.left)
+                let mid = Math.floor(lSide.length / 2)
+                let lmid = lSide[mid]
+                //console.log(lmid)
+                let leftSplits = lSide.slice(0, mid)
+                let rightSplits = lSide.slice(mid + 1, lSide.length)
+                //console.log(leftSplits)
+                //console.log(rightSplits)
+                //console.log(root.left)
+                let parent = root.left
+                let midl = Math.floor(leftSplits.length / 2)
+                let midr = Math.floor(rightSplits.length / 2)
+                let childL = new Node(leftSplits[midl])
+                parent.left = childL
+                let childR = new Node(rightSplits[midr])
+                parent.right = childR
 
-                let parent = new Node(lMid)
-                root.left = parent //everything DIES HERE
-                //buildtree(lSide)
+                childL.left = leftSplits[0]
+                childL.right = leftSplits[2]
+                childR.left = rightSplits[0]
+                childR.right = rightSplits[2]
+            }
 
-                //I would like to return the function after the while 
-                //loop until the queue is empty but I am having no luc
-                //doing so
+            
+            for(let j = 0; j <= lSide.length; j++){
                 
-            }  if (lSide <= 3 && lSide != null){
-               
-                if(lSide[0] < lSide[1]){
-                    let child = new Node(lSide[0])
-                    parent.left = child
-                } else if (lSide[0] > lSide[1]){
-                    let child = new Node(lSide[1])
-                    parent.right = child
-                } else if (rSide.length >= 3 && rSide.length != null){
+                if(rSide.length >= 3){
                     //Get the middle of the left side
                     
-                    let rMid = Math.ceil(rSide.length / 2)
-                    console.log(rMid)
-    
-                    let parent = new Node(rMid)
-                    root.left = parent
-                }
+                    let rMid = Math.floor(rSide.length / 2)
+                    //console.log(rMid)
 
-                  else if (rSide <= 3 && rSide != null){
-                    if(rSide[0] < rSide[1]){
-                        let child = new Node(rSide[0])
-                        parent.left = child
-                    } else if (rSide[0] > rSide[1]){
-                        let child = new Node(rSide[1])
-                    }
+                    //let mid = Math.floor(arr.length / 2)
+
+                    let mid = rSide[rMid]
+                    //console.log(mid)
+    
+                    let parent = new Node(mid)
+                    //console.log(parent)
+
+                    root.right = parent
+                   //console.log(parent)
+                   
                 }
-            }
-        }//while
+                
+                  if (rSide.length <= 3){
+
+                    
+
+                    if(rSide[0] < rSide[2]){
+                        let childL = new Node(rSide[0])
+                        //console.log(childL)
+                        let childR = new Node(rSide[2])
+                        //console.log(childR)
+
+                        let parent = root.right
+                       // console.log(parent)
+
+                        parent.left = childL
+                        //console.log(parent.left)
+                        parent.right = childR
+                        //console.log(parent.right)
+
+                    } else{
+                        //console.log("another test")
+                        let childR = new Node(rSide[0])
+                        let childL = new Node(rSide[2])
+                        let parent = root.right
+                        parent.right = childR
+                        parent.left = childL
+                    } 
+                } else {
+                    //console.log(root.left)
+                let mid = Math.floor(rSide.length / 2)
+    
+                
+                let leftSplits = rSide.slice(0, mid)
+                let rightSplits = rSide.slice(mid + 1, rSide.length)
+                //console.log(leftSplits)
+                //console.log(rightSplits)
+                //console.log(root.left)
+                let parent = root.right
+                let midl = Math.floor(leftSplits.length / 2)
+                let midr = Math.floor(rightSplits.length / 2)
+                let childL = new Node(leftSplits[midl])
+                parent.left = childL
+                let childR = new Node(rightSplits[midr])
+                parent.right = childR
+
+                childL.left = leftSplits[0]
+                childL.right = leftSplits[2]
+                childR.left = rightSplits[0]
+                childR.right = rightSplits[2]
+                }
+           }
+        }
         return root
         
     }//funct
@@ -96,9 +180,10 @@ class BinarySearchTree{
 
 
 const nums = [1, 2, 3, 4, 5, 6, 7]
-const bst = new BinarySearchTree(nums)
+const nums2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const bst = new BinarySearchTree(nums2)
 //console.log(root.nums)
-console.log(bst.buildtree(nums))
+console.log(bst.buildtree(nums2))
 
 
 //let queue = [[root,  [0, mid - 1]], [root, [mid + 1, nums.length - 1]]]
