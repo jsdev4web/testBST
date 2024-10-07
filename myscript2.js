bf03feb0b0647329c57139dd4ca3793cf9544794
jsweb4dev <jsweb4dev@gmail.com>
2024-10-07 11:07:30 -0400

    update

--- myscript2.js
+++ myscript2.js
@@ -16,6 +16,9 @@ class BinarySearchTree{
 
     buildtree(arr) {
 
+        let noDupes = Array.from(new Set(arr))
+        arr = noDupes
+
         //console.log(arr)
         if (arr.length <= 0) { return null } 
 
@@ -32,47 +35,111 @@ class BinarySearchTree{
 
     insert(value){
 
+        
         //establish the root
         let root = this.root
 
         //if there is no root then assign null
-        if (root === null){ return root = new Node(value) }
+        if (root.value === null){ return root = new Node(value) }
 
         //if root value === value return else
         if (root.value === value){ return root }
 
         //this variable = to the root
-        let currNode = root
+        
+        //console.log(currNode)
 
         //makes a new node
         let node = new Node(value)
-        let lastNode = currNode
+        //this will be the last node I add the insert too
+        let lastNode = null
+        let currNode = root
 
-            while(currNode.value !== null){
-            
-            //console.log(currNode, " This works ")
+            //while curr.value not null
+            while(currNode !== null){
+                lastNode = currNode
+                //console.log(lastNode, " This works ")
             if(currNode.value > value){
                 currNode = currNode.left
-            } else if (currNode.value < value) {
+            }else if (currNode.value < value) {
                 currNode = currNode.right
-            } else {
-                return currNode
+             } else {
+                return "root"
+             }
             }
 
-            if (lastNode.value > value){
-                lastNode.left = node
-            } else {
-                lastNode.right = node
-            }
+            //console.log(lastNode)
+
+
+                if (lastNode.value > value){
+                  lastNode.left = node
+                }else {
+                    lastNode.right = node
+                }
             return root
-        }   
+        }
+        
+
+        deleteItem(value){ 
+
+            let root = this.root
+
+            if(root === null) { return root }
+
+            let currNode = root
+
+        if (value === currNode.value){
+            //nodes without children
+            if (currNode.left === null && currNode.right === null ){
+                return null
+            //node with one child
+            } else if(currNode.left === null){
+                return currNode.right
+            } else if(currNode.right === null){
+                return currNode.left
+            } else {
+                // nodes with 2 children
+                //smallest right subtree
+
+                let subRight = this.smallRight(currNode.right)
+                console.log(subRight)
+                currNode.value = subRight.value
+
+                currNode.right = this.arr.deleteItem(currNode.right, subRight.value)
+            return currNode
+            }
+
+        }  /*  //recursion
+            else if(value < currNode.value){
+                currNode.left = this.deleteItem(value)
+                return currNode
+            } else if (value > currNode.value){
+                currNode.right = this.deleteItem(value)
+                return currNode
+            }*/
+            
+        } 
+
+        smallRight(node){
+            while(node !== null && node.left !== null){
+                node = node.left;
+            
+        return node
+        }
     }
+            
 }
 
-//const arr = [1, 2, 3, 4, 5, 6, 7]
-let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+//let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+
+//console.log(noDupes)
+
 const bst = new BinarySearchTree(arr)
 console.log(bst.buildtree(arr))
 bst.buildtree(arr)
 
-console.log(bst.insert(5))
+//console.log(bst.insert(11))
+
+console.log(bst.deleteItem(5))
